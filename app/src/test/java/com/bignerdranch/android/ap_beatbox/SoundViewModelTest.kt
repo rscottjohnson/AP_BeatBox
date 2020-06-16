2f4ede4d4ccce8f875be3d0dee74b47d57e6f29e
rscottjohnson <sjohnson.wvu@gmail.com>
2020-06-16 11:35:46 -0400

    Setting up your test

--- app/src/test/java/com/bignerdranch/android/ap_beatbox/SoundViewModelTest.kt
+++ app/src/test/java/com/bignerdranch/android/ap_beatbox/SoundViewModelTest.kt
@@ -3,10 +3,17 @@ package com.bignerdranch.android.ap_beatbox
 import org.junit.Before
 
 import org.junit.Assert.*
+import javax.security.auth.Subject
 
 class SoundViewModelTest {
 
+    private lateinit var sound: Sound
+    private lateinit var subject: SoundViewModel
+
     @Before
     fun setUp() {
+        sound = Sound("assetPath")
+        subject = SoundViewModel()
+        subject.sound = sound
     }
 }
